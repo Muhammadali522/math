ea2ac902d2d3cb5a90642df6b8a5eac24601fefc
Muhammadali522 <muhammadaliakmal522@gmail.com>
2026-06-07 21:00:52 +0500

    2-V

--- main.js
+++ main.js
@@ -17,17 +17,34 @@ let level = "Easy";
 
 let wrongQuestions = [];
 let correctAnswers = 0;
-const START_TIME = 250;
+
+let START_TIME = 250;
+let maxNumber = 9;
+
+const LEVELS = {
+    Easy: { max: 9, time: 200 },
+    Medium: { max: 12, time: 300 },
+    Hard: { max: 15, time: 400 },
+    ULTRA_Hard: { max: 20, time: 500 }
+};
 
 /* START */
 function startGame() {
-    
+
+    let settings = LEVELS[level];
+
+    globalTimer = settings.time;
+    START_TIME = settings.time;
+    maxNumber = settings.max;
+
+    document.getElementById("globalTimer").innerText = globalTimer;
+
+    document.getElementById("menu").style.display = "none";
+
     if (document.documentElement.requestFullscreen) {
         document.documentElement.requestFullscreen();
     }
-    
-    document.getElementById("menu").style.display = "none";
-    
+
     startCountdown();
 }
 
@@ -41,54 +58,52 @@ function startGlobalTimer() {
     globalInterval = setInterval(() => {
         globalTimer--;
         document.getElementById("globalTimer").innerText = globalTimer;
-        
+
         if (globalTimer <= 0) endGame();
     }, 1000);
 }
 
 /* GENERATE */
 function generate() {
-    let max = 9;
-    
-    if (level === "Medium") max = 12;
-    if (level === "Hard") max = 15;
-    
+
+    let max = maxNumber;
+
     let list = [];
-    
+
     for (let i = 2; i <= max; i++) {
         for (let j = i; j <= max; j++) {
             list.push([i, j]);
         }
     }
-    
+
     let available = list.filter(x => !used.has(x.join("*")));
-    
+
     if (available.length === 0) return endGame();
-    
+
     let pick = available[Math.floor(Math.random() * available.length)];
-    
+
     a = pick[0];
     b = pick[1];
     result = a * b;
-    
+
     used.add(`${a}*${b}`);
-    
+
     document.getElementById("question").innerText = `${a} × ${b}`;
-    
+
     startQuestionTimer();
 }
 
 /* TIMER */
 function startQuestionTimer() {
     clearInterval(questionInterval);
-    
+
     questionTimer = 10;
     document.getElementById("timerBox").innerText = `⏱ ${questionTimer}`;
-    
+
     questionInterval = setInterval(() => {
         questionTimer--;
         document.getElementById("timerBox").innerText = `⏱ ${questionTimer}`;
-        
+
         if (questionTimer <= 0) {
             skip_();
         }
@@ -97,23 +112,23 @@ function startQuestionTimer() {
 
 /* SHOW ANSWER */
 function showAnswer() {
-    
+
     clearInterval(questionInterval);
-    
+
     document.getElementById("answerBox").innerText = result;
     document.getElementById("overlay").style.display = "flex";
 }
 
 function correct() {
     if (answered) return;
-    
+
     score += 10;
     streak++;
     correctAnswers++;
     answered = true;
-    
+
     showScoreAnimation("+10", "plus");
-    
+
     update();
     closePopup();
     nextQuestion();
@@ -121,86 +136,86 @@ function correct() {
 
 function wrong() {
     if (answered) return;
-    
+
     score -= 5;
     streak = 0;
     answered = true;
-    
+
     wrongQuestions.push({
         a: a,
         b: b,
         result: result
     });
-    
+
     showScoreAnimation("-5", "minus");
-    
+
     update();
     closePopup();
     nextQuestion();
 }
 function skip_() {
     if (answered) return;
-    
+
     score -= 10;
     streak = 0;
     answered = false;
-    
-    
+
+
     wrongQuestions.push({
         a: a,
         b: b,
         result: result
     });
-    
+
     showScoreAnimation("-10", "minus");
-    
+
     update();
     closePopup();
     nextQuestion();
 }
 
 function startCountdown() {
-    
+
     const screen = document.getElementById("countdown");
     const text = document.getElementById("countdownText");
-    
+
     screen.style.display = "flex";
-    
+
     let numbers = ["3", "2", "1", "GO!"];
     let index = 0;
-    
+
     text.innerText = numbers[index];
-    
+
     if (numbers[index] === "GO!") {
         text.style.color = "#16a34a";
     } else {
         text.style.color = "#2563eb";
     }
-    
+
     const interval = setInterval(() => {
-        
+
         index++;
-        
+
         if (index >= numbers.length) {
-            
+
             clearInterval(interval);
-            
+
             screen.style.display = "none";
-            
+
             document.getElementById("game").style.display = "block";
-            
+
             startGlobalTimer();
             nextQuestion();
-            
+
             return;
         }
-        
+
         text.classList.remove("countAnim");
         void text.offsetWidth;
         text.classList.add("countAnim");
-        
+
         text.innerText = numbers[index];
-        
+
     }, 1000);
 }
 
@@ -229,21 +244,21 @@ function resetGame() {
 
 /* END */
 function endGame() {
-    
+
     clearInterval(globalInterval);
     clearInterval(questionInterval);
-    
+
     let usedTime = START_TIME - globalTimer;
-    
+
     let sortedWrong = wrongQuestions.sort((x, y) => {
-        
+
         if (x.a === y.a) {
             return x.b - y.b;
         }
-        
+
         return x.a - y.a;
     });
-    
+
     document.body.innerHTML = `
     
     <div class="menu">
@@ -267,9 +282,9 @@ function endGame() {
         <h2>Wrong Questions</h2>
     
         ${sortedWrong.length
-    ? sortedWrong.map(q => `<p>${q.a} × ${q.b} = ${q.result}</p>`).join("")
-    : "<p>No mistakes 🎉</p>"
-}
+            ? sortedWrong.map(q => `<p>${q.a} × ${q.b} = ${q.result}</p>`).join("")
+            : "<p>No mistakes 🎉</p>"
+        }
 
         <button class="blue" onclick="location.reload()">
             Reset
@@ -280,20 +295,20 @@ function endGame() {
     `;
 }
 function showScoreAnimation(text, type) {
-    
+
     const scoreBox = document.getElementById("scoreContainer");
     const rect = scoreBox.getBoundingClientRect();
-    
+
     const div = document.createElement("div");
-    
+
     div.className = "scoreEffect " + type;
     div.innerText = text;
-    
+
     div.style.left = (rect.left + rect.width / 2 - 15) + "px";
     div.style.top = (rect.top + 30) + "px";
-    
+
     document.body.appendChild(div);
-    
+
     setTimeout(() => {
         div.remove();
     }, 1000);
